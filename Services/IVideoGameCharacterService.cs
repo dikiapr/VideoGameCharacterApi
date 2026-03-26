0c3f11aac25f2610506fb9106b7e80c747c82782
diki <adiki436@gmail.com>
2026-03-26 23:32:23 +0700

    finish

---
+++ Services/IVideoGameCharacterService.cs
@@ -0,0 +1,17 @@
+using VideoGameCharacterApi.Dtos;
+using VideoGameCharacterApi.Models;
+
+namespace VideoGameCharacterApi.Services;
+
+public interface IVideoGameCharacterService
+{
+    Task<List<CharacterResponse>> GetAllCharactersAsync();
+
+    Task<CharacterResponse?> GetCharacterByIdAsync(int id);
+
+    Task<CharacterResponse> AddCharacterAsync(CreateCharacterRequest character);
+
+    Task<bool> UpdateCharacterAsync(int id, UpdateCharacterRequest character);
+
+    Task<bool> DeleteCharacterAsync(int id);
+}
